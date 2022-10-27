e5421d117b053ca91d642e8a04364b2fd0d69423
brigantinojoe <103912190+brigantinojoe@users.noreply.github.com>
2022-10-27 15:03:56 -0700

    Add for loop in script.js to determine if the html time slot is before or after moment() time. Conditional formmating changes row colors depending on if the html time slot is in the past, present, or future.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,46 @@
 // Use JQUERY for everything
-var currentDay = $("#currentDay");
 // TODO: Add elements
+var currentDay = $("#currentDay");
+var timeBlock = $(".time-block");
+var col9AM = $(".09AM");
+var col10AM = $(".10AM");
+var col11AM = $(".11AM");
+var col12PM = $(".12PM");
+var col1PM = $(".13PM");
+var col2PM = $(".14PM");
+var col3PM = $(".15PM");
+var col4PM = $(".16PM");
+var col5PM = $(".17PM");
+
+var timeSlotArray = [col9AM, col10AM, col11AM, col12PM, col1PM, col2PM, col3PM, col4PM, col5PM];
+
+// Get Time Block time and set block time.
+for (let i = 0; i < timeSlotArray.length; i++) {
+    const element = timeSlotArray[i][0];
+    var timeText = $(element).find("p").text();
+    var timeHH = moment(timeText, "hh").format("HH");
+    var nowHH = moment().format("HH");
+    console.log(timeText);
+    console.log(timeHH);
+    console.log(nowHH);
+
+    $(element).find("p").text(moment(timeText, "hh:mm").format("hh:mm a"));
+
+    if (timeHH === nowHH) {
+        $(element).next().addClass("present");
+    } else if (timeHH > nowHH) {
+        $(element).next().addClass("future");
+    }
+}
 
 // TODO: Add setInterval function for the date. Weekday, Month Date (st/th)
 currentDay.text(moment().format('dddd, MMMM Do'));
 
-// TODO: Create timeblocks for each hour of the day.
-// Timeblocks should be colored based on past, present, and future
+// TODO: Timeblocks should be colored based on past, present, and future
+    // Determine Current Time
+    // Determine time block time
+        // Maybe format the text in time block? format("HH")?
+    // Compare Current/Block Times
+    // Return background-color
 
-// TODO: Add event listener so when you click on a time block, you can add text. 
 // Text should be added to local storage. When the user clicks the "Save" button.
