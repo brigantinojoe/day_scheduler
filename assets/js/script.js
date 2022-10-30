54eb695345d84689c6d7805bf3d2ec230989d6b6
brigantinojoe <103912190+brigantinojoe@users.noreply.github.com>
2022-10-30 12:36:24 -0700

    Add event listener to target the text box content to extract user input and save to local storage. Use for loop to loop through localStorage object to display saved content when the page is refreshed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ var col2PM = $(".14PM");
 var col3PM = $(".15PM");
 var col4PM = $(".16PM");
 var col5PM = $(".17PM");
+var button = $("button");
 
 var timeSlotArray = [col9AM, col10AM, col11AM, col12PM, col1PM, col2PM, col3PM, col4PM, col5PM];
 
@@ -20,9 +21,6 @@ for (let i = 0; i < timeSlotArray.length; i++) {
     var timeText = $(element).find("p").text();
     var timeHH = moment(timeText, "hh").format("HH");
     var nowHH = moment().format("HH");
-    console.log(timeText);
-    console.log(timeHH);
-    console.log(nowHH);
 
     $(element).find("p").text(moment(timeText, "hh:mm").format("hh:mm a"));
 
@@ -37,3 +35,31 @@ for (let i = 0; i < timeSlotArray.length; i++) {
 currentDay.text(moment().format('dddd, MMMM Do'));
 
 // Text should be added to local storage. When the user clicks the "Save" button.
+button.click(function (e) {
+    e.preventDefault();
+    var parent = $(e.target).parent();
+    var textBox = parent.children("div").eq(1);
+    var time = parent.children("div").eq(0).text();
+    var timeClass = moment(time, "HH").format("HH");
+    if (parseInt(timeClass) < 8) {
+        var timeNumber = parseInt(timeClass) + 12;
+        var timeClass = timeNumber.toString();
+    }
+    if (parseInt(timeClass) > 11) {
+        timeClass += "PM-text";
+    } else { 
+        timeClass += "AM-text";
+    }
+    console.log(timeClass);
+    var textBoxContent = textBox.text();
+    localStorage.setItem(timeClass, textBoxContent);
+});
+
+for (const key in localStorage) {
+    if (localStorage.hasOwnProperty.call(localStorage, key)) {
+        const element = localStorage[key];
+        $(`.${key}`).find("p").text(element);
+        console.log(key);
+        console.log(element);
+    }
+}
